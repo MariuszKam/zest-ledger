02b8e0e9909445612a151f6e84009d494f560fd8
Mariusz <87607813+MariuszKam@users.noreply.github.com>
2025-09-15 22:43:58 +0200

    feat(db): postgrest (#24)
    * build(gradle): add and refactor catalog version
    
    * build(gradle): add dependencies to adapter-out
    
    * chore(config): add junit properties

--- modules/adapters-out/db/build.gradle.kts
+++ modules/adapters-out/db/build.gradle.kts
@@ -6,4 +6,16 @@ plugins {
 
 dependencies {
 	implementation(project(":modules:domain"))
+	implementation(platform(libs.spring.boot.bom))
+	implementation(libs.spring.boot.starter.data.jpa)
+	implementation(libs.flyway.core)
+
+	runtimeOnly(libs.flyway.postgresql)
+	runtimeOnly(libs.postgresql)
+
+	testImplementation(libs.spring.boot.starter.test)
+	testImplementation(libs.spring.boot.testcontainers)
+	testImplementation(platform(libs.testcontainers.bom))
+	testImplementation(libs.testcontainers.junit)
+	testImplementation(libs.testcontainers.postgres)
 }
