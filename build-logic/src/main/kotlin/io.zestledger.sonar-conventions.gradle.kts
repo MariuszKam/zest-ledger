e8c71b6d98b8276f88b1c10b0f82e323884fe0a9
MariuszKam <rexxarhell@gmail.com>
2025-09-17 00:48:24 +0200

    fix(gha): fix sonar cloud in gha

--- build-logic/src/main/kotlin/io.zestledger.sonar-conventions.gradle.kts
+++ build-logic/src/main/kotlin/io.zestledger.sonar-conventions.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 sonar {
 	properties {
 		property("sonar.organization", "mariuszkam")
-		property("sonar.projectKey", "mariuszkam_zest-ledger")
+		property("sonar.projectKey", "MariuszKam_zest-ledger")
 		property("sonar.projectName", "zest-ledger")
 		property("sonar.exclusions", "modules/architecture-tests/**")
 		property("sonar.java.source", "21")
