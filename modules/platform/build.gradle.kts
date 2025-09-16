5039857ecf308c02bea958b870f532f7bc394f49
MariuszKam <rexxarhell@gmail.com>
2025-09-16 12:30:55 +0200

    feat(spring): add spring boot, web actuator to platform

--- modules/platform/build.gradle.kts
+++ modules/platform/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
 	id("io.zestledger.java-conventions")
 	id("io.zestledger.spotless-conventions")
+	id("io.zestledger.spring-boot-conventions")
 }
 
 dependencies {
 	implementation(project(":modules:domain"))
 	implementation(project(":modules:application"))
+	implementation(platform(libs.spring.boot.bom))
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.spring.boot.starter.actuator)
+	testImplementation(libs.spring.boot.starter.test)
 }
