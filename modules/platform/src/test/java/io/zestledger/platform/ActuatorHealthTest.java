334b3ac83691c9708235e82d32439a3788b14a0e
MariuszKam <rexxarhell@gmail.com>
2025-09-17 23:51:09 +0200

    chore(platform): format test

--- modules/platform/src/test/java/io/zestledger/platform/ActuatorHealthTest.java
+++ modules/platform/src/test/java/io/zestledger/platform/ActuatorHealthTest.java
@@ -1,5 +1,7 @@
 package io.zestledger.platform;
 
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,20 +9,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class ActuatorHealthTest {
 
-	@Test
-	@DisplayName("Should return status Up when endpoint is called")
-	void shouldReturnUpStatusWhenHealthEndpointIsCalled(@Autowired WebTestClient client) {
-		client.get().uri("/actuator/health")
-			.exchange()
-			.expectStatus().isOk()
-			.expectHeader().contentTypeCompatibleWith(MediaType.valueOf("application/*+json"))
-			.expectBody().jsonPath("status").isEqualTo("UP");
-
-	}
-
+    @Test
+    @DisplayName("Should return status Up when endpoint is called")
+    void shouldReturnUpStatusWhenHealthEndpointIsCalled(@Autowired WebTestClient client) {
+        client.get()
+                .uri("/actuator/health")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .contentTypeCompatibleWith(MediaType.valueOf("application/*+json"))
+                .expectBody()
+                .jsonPath("status")
+                .isEqualTo("UP");
+    }
 }
