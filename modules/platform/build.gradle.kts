7452cb4994e8c92c15ae047a6858cb53733acbf7
Mariusz <87607813+MariuszKam@users.noreply.github.com>
2025-09-17 23:54:09 +0200

    test(platform): verify actuator health with WebTestClient (#32)
    * feat(platform): add webflux
    
    * test(platform): verify actuator health with WebTestClient
    
    * chore(platform): format test

--- modules/platform/build.gradle.kts
+++ modules/platform/build.gradle.kts
@@ -12,5 +12,6 @@ dependencies {
 	implementation(platform(libs.spring.boot.bom))
 	implementation(libs.spring.boot.starter.web)
 	implementation(libs.spring.boot.starter.actuator)
+	implementation(libs.spring.boot.starter.webflux)
 	testImplementation(libs.spring.boot.starter.test)
 }
---
+++ modules/platform/src/test/java/io/zestledger/platform/ActuatorHealthTest.java
@@ -0,0 +1,29 @@
+package io.zestledger.platform;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class ActuatorHealthTest {
+
+    @Test
+    @DisplayName("Should return status Up when endpoint is called")
+    void shouldReturnUpStatusWhenHealthEndpointIsCalled(@Autowired WebTestClient client) {
+        client.get()
+                .uri("/actuator/health")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectHeader()
+                .contentTypeCompatibleWith(MediaType.valueOf("application/*+json"))
+                .expectBody()
+                .jsonPath("status")
+                .isEqualTo("UP");
+    }
+}
