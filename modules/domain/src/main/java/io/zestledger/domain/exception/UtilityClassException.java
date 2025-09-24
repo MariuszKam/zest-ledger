6780c4d708e4c60d1b1730994abc0b4b432171b7
MariuszKam <rexxarhell@gmail.com>
2025-09-24 17:20:57 +0200

    chore(utility): move exception to utils and change access modifier to default

--- modules/domain/src/main/java/io/zestledger/domain/exception/UtilityClassException.java
+++
@@ -1,7 +0,0 @@
-package io.zestledger.domain.exception;
-
-public class UtilityClassException extends RuntimeException {
-	public UtilityClassException() {
-		super("Utility Class cannot create instance");
-	}
-}
---
+++ modules/domain/src/main/java/io/zestledger/domain/utils/UtilityClassException.java
@@ -0,0 +1,7 @@
+package io.zestledger.domain.utils;
+
+class UtilityClassException extends RuntimeException {
+	public UtilityClassException() {
+		super("Utility Class cannot create instance");
+	}
+}
